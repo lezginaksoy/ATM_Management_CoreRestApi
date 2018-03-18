d2ae8488d496f2d669759d3efee8ddfb675dfba1
lezginaksoy <lezginaksoy@gmail.com>
2018-03-18 11:15:24 +0300

    ef code first

--- ATM_Management_CoreRestApi/Program.cs
+++ ATM_Management_CoreRestApi/Program.cs
@@ -7,6 +7,7 @@ using ATM_Management_CoreRestApi.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ATM_Management_CoreRestApi
@@ -18,24 +19,35 @@ namespace ATM_Management_CoreRestApi
 
             BuildWebHost(args).Run();
 
-            //var host = new WebHostBuilder()
-            //    .UseKestrel()
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .UseIISIntegration()
-            //    .UseStartup<Startup>()
-            //    .UseApplicationInsights()
-            //    .Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
 
-            //host.Run();
+            host.Run();
 
         }
 
 
-        public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //WebHost.CreateDefaultBuilder(args)
+        //    .UseStartup<Startup>()
+        //    .Build();
 
+        public static IWebHost BuildWebHost(string[] args) =>
+         WebHost.CreateDefaultBuilder(args)
+         .UseStartup<Startup>()
+             .ConfigureAppConfiguration((hostContext, config) =>
+             {
+                    // delete all default configuration providers
+                    //config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                    config.Sources.Clear();
+                 config.AddJsonFile("appsettings.json", optional: true);
+             })
+         .Build();
 
 
     }
